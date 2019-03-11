5e131a418d5481718a5b0b7afc3ca35513cdead2
Bailie Wu <wubailie@gmail.com>
2019-03-11 03:14:02 -0400

    order of users & things same as prof

--- src/Things.js
+++ src/Things.js
@@ -13,7 +13,15 @@ export default class Things extends Component{
                             <ul>
                                 {
                                     favorites.filter( (fav) => fav.thingId === thing.id).map( (fav) => {
-                                    return (<li key={fav.id}>favorited by: {users[fav.userId - 1].name} </li>)
+                                    // return (<li key={fav.id}>favorited by: {users[fav.userId - 1].name} </li>)
+                                    return (
+                                        users.filter( (user) => user.id === fav.userId)
+                                            .map( (user2) => {
+                                                return (
+                                                    <li key={fav.id}> favorited by: {user2.name}</li>
+                                                )
+                                            })
+                                        )
                                     })
                                 }
                             </ul>
--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 
 export default class Users extends Component{
+
     
     render(){
         const {users, things, favorites} = this.props
+
         return (
             <ul>
                 {users.map( (user) => {
@@ -13,7 +15,15 @@ export default class Users extends Component{
                             <ul>
                                 {
                                     favorites.filter( (fav) => fav.userId === user.id).map( (fav) => {
-                                    return (<li key={fav.id}>{things[fav.thingId - 1].name} (Ranked: {fav.rank})</li>)
+                                    // return (<li key={fav.id}>{things[fav.thingId - 1].name} (Ranked: {fav.rank})</li>)
+                                    return (
+                                        things.filter( (thing) => thing.id === fav.thingId)
+                                            .map( (thing2) => {
+                                                return (
+                                                    <li key={fav.id}> {thing2.name}(Ranked: {fav.rank})</li>
+                                                )
+                                            })
+                                        )
                                     })
                                 }
                             </ul>
