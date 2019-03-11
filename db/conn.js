a83f1ce0dd703e064a51092828c29dadfa9eba25
Bailie Wu <wubailie@gmail.com>
2019-03-11 01:57:20 -0400

    db connection

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const conn = new Sequelize(process.env.PORT || process.env.DATABASE_URL || 'postgres://localhost/acme_favorite_spa', {
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_favorite_spa', {
     logging: false
 })
 
